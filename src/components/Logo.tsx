5050a7aaf9368440ae8892e74a07e7bca4e075a8
abhyudaysingh <hospitoservice@gmail.com>
2025-11-16 03:33:39 +0530

    Experimental commit

---
+++ src/components/Logo.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-scroll';
+import logo from '../asset/hospitonetLogoPng.png';
+
+interface LogoProps {
+  className?: string;
+  height?: number;
+  onClick?: () => void;
+}
+
+const Logo: React.FC<LogoProps> = ({ className = '', height = 50, onClick }) => {
+  return (
+    <Link 
+      to="home" 
+      className={`logo-link ${className}`}
+      smooth={true}
+      duration={500}
+      offset={-80}
+      onClick={onClick}
+    >
+      <img 
+        src={logo} 
+        alt="Hospitonet Logo" 
+        className="logo"
+        style={{ height: `${height}px`, width: 'auto' }}
+      />
+    </Link>
+  );
+};
+
+export default Logo;
